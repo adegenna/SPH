dd0947c8cd4d5ab202ee394861480a61a8fad140
adegenna <adegenna@princeton.edu>
2012-12-05 20:36:53 -0500

    Added capability to add neighbors to a particle object

--- particle.h
+++ particle.h
@@ -5,17 +5,24 @@ class Particle {
  public:
   Particle(int tag, double* x, double* v, double mass, double visc);
   ~Particle();
-  double Distance(double* location);    // Return distance to particle
-  void GetLocation(double* location);   // Return r_
-  void GetVelocity(double* velocity);   // Return v_
-  void SetLocation(double* location);   // Update r_
-  void SetVelocity(double* velocity);   // Update v_
+  double Distance(double* location);           // Return distance to particle
+  void GetLocation(double* location);          // Return r_
+  void GetVelocity(double* velocity);          // Return v_
+  void SetLocation(double* location);          // Update r_
+  void SetVelocity(double* velocity);          // Update v_
+  void AddNeighbor(Particle* neighbor);        // Add a neighbor
+  int Number_of_Neighbors();                // Return neighbors_
+  void GetNeighbors(Particle** neighbor);      // Get neighbor_pointer_
+  int GetTag();                                // Return tag_
+  
  private:
-  double* x_;     // Location Pointer
-  double* v_;     // Velocity Pointer
-  double mass_;   // Particle Mass
-  double visc_;   // Particle Viscosity
-  int tag_;       // Particle identifier
+  double* x_;                    // Location Pointer
+  double* v_;                    // Velocity Pointer
+  double mass_;                  // Particle Mass
+  double visc_;                  // Particle Viscosity
+  int tag_;                      // Particle identifier
+  int neighbors_;                // Number of neighbors
+  Particle** neighbor_pointer_;  // Pointer to neighbor chain
 };
 
 #endif  // PARTICLE_H
