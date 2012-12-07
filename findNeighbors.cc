8472c98ac46c2a0adf1da9d26993b1baeb9a2a65
Kevin Nowland <knowland@princeton.edu>
2012-12-07 13:36:09 -0500

    Filled in version of findNeighbors.cc

--- findNeighbors.cc
+++ findNeighbors.cc
@@ -1,11 +1,16 @@
 #include "findNeighbors.h"
 
 void findNeighbors(Particle *particles){
+    float dist;
     for(int i=0; i<NUMPARTICLES; ++i){
         for(int j=i; j<NUMPARTICLES; ++j){
             // calculate some distances
-            // add particle j to the neighbors of i
-            // add particle i to the neighbors of j
+            dist = particles[i]->Distance(particles[j]->GetLocation);
+            if(dist < SMOOTHINGLENGTH){
+                // add neighbor
+                particles[i]->AddNeighbor(particles[j]);
+                particles[j]->AddNeighbor(particles[i]);
+            }
         }
     }
 }
