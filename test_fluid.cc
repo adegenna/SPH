812c5c17e2e7d1dec81e287ff338a1a3908cc62b
Kevin Nowland <knowland@princeton.edu>
2012-12-12 15:43:43 -0500

    Created initial version of test_fluid.cc

---
+++ test_fluid.cc
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <iostream>
+#include <gtest/gtest.h>
+
+#include <fluid.h>
+#include <particle.h>
+
+
+int main(int argc, char** argv){
+  // program to test the fluid and particle classes
+  
+  cout << "Fluid and particle testing:" << endl;
+
+
+  return 0;
+}
