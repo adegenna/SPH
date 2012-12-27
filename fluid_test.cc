f1f141e3731017e80711749519c5504ce035490d
Kevin Nowland <knowland@princeton.edu>
2012-12-27 18:02:14 -0500

    Implemented fluid_test.cc

--- fluid_test.cc
+++ fluid_test.cc
@@ -1,22 +1,123 @@
 #include "fluid_test.h"
 
-TEST (FluidTest,checkAddParticle){}
+TEST_F (FluidTest,checkAddGetParticle){
+  fluid_->addParticle(0,props1_);
+  fluid_->addParticle(1,props2_);
 
-TEST (FluidTest,checkGetParticles){}
+  Particle **parts = new Particle*[2];
+  fluid_->getParticles(parts);
 
-TEST (FluidTest,checkAddBoundary){}
+  EXPECT_EQ(0,parts[0]->getTag());
+  EXPECT_EQ(1,parts[1]->getTag());
 
-TEST (FluidTest,checkGetBoundaries){}
+  delete parts;
+}
 
-TEST (FluidTest,checkFindNeighbors){}
+TEST_F (FluidTest,checkAddGetBoundary){
+  fluid_->addBoundary(0,propsb_);
 
-TEST (FluidTest,checkResetNeighbors){}
+  Particle **parts = new Particle*[1];
+  fluid_->getBoundaries(parts);
 
-TEST (FluidTest,checkGetKernel){}
+  EXPECT_EQ(0,parts[0]->getTag());
 
-TEST (FluidTest,checkGetNParticles){}
+  delete parts;
+}
 
-TEST (FluidTest,checkGetNBoundaries){}
+TEST_F (FluidTest,checkFindNeighbors){
+  // NOTE: find neighbors is not working like I expected
+  // it is working how it is written, but I expected something 
+  // more along the lines of it listing all neighbors from lowest
+  // tag to highest tag, including the self, which it is not written
+  // that way. Need to talk to others about this. -- Kevin
+  initFluid();
+  Particle **parts = new Particle*[2];
+  int neighbors[2];
 
-TEST (FluidTest,checkResetParticles){}
+  fluid_->findNeighbors();
+  fluid_->getParticles(parts);
+
+  parts[0]->getNeighbors(neighbors);
+  EXPECT_EQ(0,neighbors[0]);
+  EXPECT_EQ(1,neighbors[1]);
+
+  parts[1]->getNeighbors(neighbors);
+  EXPECT_EQ(0,neighbors[0]);
+  EXPECT_EQ(1,neighbors[1]);
+
+  delete parts, neighbors;
+}
+
+TEST_F (FluidTest,checkResetNeighbors){
+  initFluid();
+  Particle **parts = new Particle*[2];
+  int neighbors[2];
+
+  // if findNeighbors is working (see above test)
+  // then this should produce different data
+  fluid_->findNeighbors();
+  fluid_->resetNeighbors();
+  fluid_->getParticles(parts);
+
+  parts[0]->getNeighbors(neighbors);
+  EXPECT_EQ(-1,neighbors[0]);
+  EXPECT_EQ(-1,neighbors[1]);
+
+  parts[1]->getNeighbors(neighbors);
+  EXPECT_EQ(-1,neighbors[0]);
+  EXPECT_EQ(-1,neighbors[1]);
+
+  delete parts, neighbors;
+}
+
+TEST_F (FluidTest,checkGetKernel){
+  Kernel *kernel;
+  kernel = fluid_->getKernel();
+
+  // this is here only to assert that Fluid::getKernel works,
+  // as the destructor below is not working (not sure why)
+  double x = -99999;
+  EXPECT_NE(x,kernel->W(1));
+
+  // the delete below is causing a seg fault
+  // the destructors seem to be in place, but there is no kernel
+  // constructor, so maybe that's it??
+
+  //delete kernel;
+}
+
+TEST_F (FluidTest,checkGetNParticles){
+  initFluid();
+  EXPECT_EQ(2,fluid_->getNParticles());
+}
+
+TEST_F (FluidTest,checkGetNBoundaries){
+  initFluid();
+  EXPECT_EQ(1,fluid_->getNBoundaries());
+}
+
+TEST_F (FluidTest,checkResetParticles){
+  initFluid();
+  Properties props1 = props1_;
+  Properties props2 = props2_;
+  Properties props3;
+
+  props1.x = props1_.x + 1;
+  props2.density = props2_.density - 1;
+
+  Particle **parts1 = new Particle*[2];
+  Particle **parts2 = new Particle*[2];
+  parts1[0] = new Particle(0,2,props1);
+  parts1[1] = new Particle(1,2,props2);
+
+  fluid_->resetParticles(parts1);
+  fluid_->getParticles(parts2);
+
+  parts2[0]->get("OLD",props3);
+  EXPECT_FLOAT_EQ(props3.x,props1_.x+1);
+  parts2[1]->get("OLD",props3);
+  EXPECT_FLOAT_EQ(props3.density,props1_.density-1);
+
+  delete parts1, parts2;
+}
 
--- fluid_test.h
+++ fluid_test.h
@@ -9,8 +9,57 @@
 
 class FluidTest : public ::testing::Test{
   protected:
-    FluidTest(){}
-    ~FluidTest(){}
+    virtual void SetUp(){
+      smoothinglength_ = 1;
+      kernel_ = new GaussianKernel(smoothinglength_);
+
+      props1_.x = 1;
+      props1_.y = -1;
+      props1_.u = 0;
+      props1_.v = 0.1;
+      props1_.density = 1;
+      props1_.mass = 1;
+      props1_.pressure = 2;
+      props1_.visc = 1;
+
+      props2_.x = 1;
+      props2_.y = -1.0001;
+      props2_.u = 0;
+      props2_.v = -0.1;
+      props2_.density = 1;
+      props2_.mass = 1;
+      props2_.pressure = 2;
+      props2_.visc = 1;
+
+      propsb_.x = 0;
+      propsb_.y = 0;
+      propsb_.u = 0;
+      propsb_.v = 0;
+      propsb_.density = 100;
+      propsb_.mass = 100;
+      propsb_.pressure = 0;
+      propsb_.visc = 100;
+
+      fluid_ = new Fluid(kernel_,2,1,smoothinglength_);
+    }
+
+    virtual void TearDown(){
+      delete kernel_, fluid_;
+    }
+
+    virtual void initFluid(){
+      // helper function for some tests
+      fluid_->addParticle(0,props1_);
+      fluid_->addParticle(1,props2_);
+      fluid_->addBoundary(0,propsb_);
+    }
+
+    Properties props1_, props2_, propsb_;
+    Fluid *fluid_;
+    double smoothinglength_;
+
+  private:
+    Kernel *kernel_;
 };
 
 #endif // FLUID_TEST_H_
