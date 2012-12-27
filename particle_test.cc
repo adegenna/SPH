875e57ba19f88cf8fffd73a57ca8569fdb8f731a
Kevin Nowland <knowland@princeton.edu>
2012-12-27 16:29:17 -0500

    Made particle_test use test fixtures (common test initialization)

--- particle_test.cc
+++ particle_test.cc
@@ -1,23 +1,40 @@
 #include "particle_test.h"
 
-TEST (ParticleTest,checkGetSet){
-  Properties props1, props2;
-  props1.x = 1.25;
-  props1.y = -12.4;
-  props1.u = -.5;
-  props1.v = 0.0003;
-  props1.density = 1.0;
-  props1.mass = 0.0;
-  props1.pressure = -2039;
-  props1.visc = 13;
+TEST_F (ParticleTest,checkGetSet){
+  // check Particle::get
+  ASSERT_EQ(0,part1_->get("OLD",props2_));
+  EXPECT_FLOAT_EQ(1.25,props2_.x);
+  EXPECT_FLOAT_EQ(-12.4,props2_.y);
+  EXPECT_FLOAT_EQ(-.5,props2_.u);
+  EXPECT_FLOAT_EQ(0.0003,props2_.v);
+  EXPECT_FLOAT_EQ(1.0,props2_.density);
+  EXPECT_FLOAT_EQ(0.0,props2_.mass);
+  EXPECT_FLOAT_EQ(-2039,props2_.pressure);
+  EXPECT_FLOAT_EQ(13,props2_.visc);
+  
+  // check Particle::set
+  props2_.x = 0.0;
+  ASSERT_EQ(0,part1_->set("NEW",props2_));
+  ASSERT_EQ(0,part1_->get("NEW",props1_));
+  EXPECT_EQ(props1_.x,props2_.x);
 }
 
-TEST (ParticleTest,checkAddNeighbor){}
+TEST_F (ParticleTest,checkNeighbors){
+  // check numberOfNeighbors is initially 0
+  EXPECT_EQ(0,part1_->numberOfNeighbors());
 
-TEST (ParticleTest,checkDeleteNeighbors){}
+  // check that there is now 1 neighbor
+  part1_->addNeighbor(part2_);
+  EXPECT_EQ(1,part1_->numberOfNeighbors());
 
-TEST (ParticleTest,checkNumberOfNeighbors){}
+  // check that there are now 0 neighbors
+  part1_->deleteNeighbors();
+  EXPECT_EQ(0,part1_->numberOfNeighbors());
+}
 
-TEST (ParticleTest,checkGetNeighbors){}
+TEST_F (ParticleTest,checkGetTag){
 
-TEST (ParticleTest,checkGetTag){}
+  EXPECT_EQ(1,part1_->getTag());
+  EXPECT_EQ(2,part2_->getTag());
+
+}
--- particle_test.h
+++ particle_test.h
@@ -7,8 +7,32 @@
 
 class ParticleTest : public ::testing::Test{
   protected:
-    ParticleTest(){}
-    ~ParticleTest(){}
+    virtual void SetUp(){
+      props1_.x = 1.25;
+      props1_.y = -12.4;
+      props1_.u = -.5;
+      props1_.v = 0.0003;
+      props1_.density = 1.0;
+      props1_.mass = 0.0;
+      props1_.pressure = -2039;
+      props1_.visc = 13;
+      props2_.x = -3.25;
+      props2_.y = 1.34;
+      props2_.u = -98;
+      props2_.v = -1.22;
+      props2_.density = 2.7;
+      props2_.mass = 1.005;
+      props2_.pressure = 0.005;
+      props2_.visc = 2;
+
+      part1_ = new Particle(1,2,props1_);
+      part2_ = new Particle(2,2,props2_);
+    }
+
+    virtual void TearDown(){ delete part1_, part2_; }
+
+    Properties props1_, props2_;
+    Particle *part1_, *part2_;
 };
 
 #endif // PARTICLE_TEST_H_
